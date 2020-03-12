35095889d2e088722e8f65d8f256dced1336066c
Gabriel Consalter <consaltergabriel@gmail.com>
2020-03-12 14:53:43 -0300

    Added a delayed response from server

--- server/index.js
+++ server/index.js
@@ -9,7 +9,15 @@ const
 io.on('connection', socket => {
 	console.log('A user connected');
 
-	socket.on('newMessage', message => console.log(`New message: ${message}`));
+	socket.on('newMessage', message => {
+		console.log(`New message: ${message}`);
+
+		setTimeout(() => io.emit('newMessage', {
+			text: `I received your message: ${message.text}`,
+			date: new Date(),
+			sended: false
+		}) ,2000);
+	});
 });
 
 server.listen(port, () => console.log(`Server running on port ${port}`));
