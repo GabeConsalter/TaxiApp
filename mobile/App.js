c2698334e60bd3c60140b33cca1eadfca34cae62
Gabriel Consalter <consaltergabriel@gmail.com>
2020-03-12 10:20:08 -0300

    Added socket io connection

--- mobile/App.js
+++ mobile/App.js
@@ -1,15 +1,12 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
-import React from 'react';
+import React, { useEffect } from 'react';
 import { View, Text } from 'react-native';
+import io from 'socket.io-client';
 
 export default function App() {
+  useEffect(() => {
+    const socket = io('http://192.168.0.130:3000');
+  }, []);
+
   return (
     <>
       <View>
