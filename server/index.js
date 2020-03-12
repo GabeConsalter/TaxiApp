a6792df3e17aefa561baf10c3e7808c4766f95b2
Gabriel Consalter <consaltergabriel@gmail.com>
2020-03-12 10:06:11 -0300

    First Commit

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const
+	express = require('express'),
+	app = express(),
+	server = require('http').createServer(app),
+	io = require('socket.io'),
+	port = 3000
+;
+
+io.on('connection', socket => {
+	console.log('A user connected');
+});
+
+server.listen(port, () => console.log(`Server running on port ${port}`));
