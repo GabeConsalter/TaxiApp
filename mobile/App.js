e06b4f3493ab1e36519075ecddbf3d962d0346a2
Gabriel Consalter <consaltergabriel@gmail.com>
2020-03-12 10:28:31 -0300

    Send a message UI and styles

--- mobile/App.js
+++ mobile/App.js
@@ -1,16 +1,46 @@
 import React, { useEffect } from 'react';
-import { View, Text } from 'react-native';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
 import io from 'socket.io-client';
 
 export default function App() {
+  const styles = StyleSheet.create({
+    container: {
+      alignItems: 'center',
+      flex: 1,
+      padding: 16
+    },
+
+    textInput: {
+      height: 60,
+      backgroundColor: '#EFEFEF',
+      width: '100%',
+      paddingHorizontal: 8,
+      borderRadius: 7,
+      position: 'absolute',
+      bottom: 16
+    },
+
+    title: {
+      backgroundColor: '#383838',
+      paddingHorizontal: 8,
+      paddingVertical: 4,
+      fontWeight: 'bold',
+      color: '#FFF',
+      borderRadius: 7
+    }
+  });
+
   useEffect(() => {
     const socket = io('http://192.168.0.130:3000');
   }, []);
 
   return (
     <>
-      <View>
-        <Text>Hello TaxiApp</Text>
+      <View style={styles.container}>
+        <Text style={styles.title}>TaxiApp</Text>
+        <TextInput
+          style={styles.textInput}
+          placeholder="Send a message" />
       </View>
     </>
   );
