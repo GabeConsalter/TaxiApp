fe32f5560ecf194d71c28dc48248d21b63905acf
Gabriel Consalter <consaltergabriel@gmail.com>
2020-03-12 11:04:43 -0300

    Messages received

--- server/index.js
+++ server/index.js
@@ -8,6 +8,8 @@ const
 
 io.on('connection', socket => {
 	console.log('A user connected');
+
+	socket.on('newMessage', message => console.log(`New message: ${message}`));
 });
 
 server.listen(port, () => console.log(`Server running on port ${port}`));
